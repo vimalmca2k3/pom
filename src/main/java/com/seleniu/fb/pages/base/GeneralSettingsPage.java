68d65b94c3ff013cd5878a93a9e9a05ce24fc3e7
Vimal <vimalmca2k3@gmail.com>
2019-06-11 13:58:09 -0400

    Added GeneralSettingsPage and TopMenu.java

---
+++ src/main/java/com/seleniu/fb/pages/base/GeneralSettingsPage.java
@@ -0,0 +1,15 @@
+package com.seleniu.fb.pages.base;
+
+import org.openqa.selenium.WebDriver;
+
+import com.relevantcodes.extentreports.ExtentTest;
+
+public class GeneralSettingsPage extends BasePage {
+
+	
+	
+	public GeneralSettingsPage(WebDriver driver,ExtentTest test){
+		this.driver=driver;
+		this.test=test;
+	}
+}
---
+++ src/main/java/com/seleniu/fb/pages/base/TopMenu.java
@@ -0,0 +1,58 @@
+package com.seleniu.fb.pages.base;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+import com.seleniu.fb.util.FBConstants;
+
+public class TopMenu {
+	
+	
+	@FindBy(xpath=FBConstants.NAVIGATION_LABEL)
+	public WebElement navigationLabel;
+	@FindBy(xpath=FBConstants.SETTINGS_LINK)
+	
+	public WebElement settings;
+	ExtentTest test;
+	
+	
+	WebDriver driver;
+	
+	
+	TopMenu(WebDriver driver,ExtentTest test)
+	{
+		
+		this.driver = driver;
+		this.test = test;
+		
+		
+	}
+
+ public void logout(){
+		
+	}
+
+    /*
+    public GeneralSettingsPage gotoSettings(){
+	test.log(LogStatus.INFO, "Going to settings");
+	//navigationLabel.click();
+	JavascriptExecutor js = (JavascriptExecutor)driver;
+	js.executeScript("document.getElementById('userNavigationLabel').click()");
+	settings.click();
+	test.log(LogStatus.INFO, "Settings page opened");
+	GeneralSettingsPage settings =new GeneralSettingsPage(driver,test);
+	PageFactory.initElements(driver, settings);
+	return settings;
+}
+*/
+public void search(){
+	
+}
+
+
+}
